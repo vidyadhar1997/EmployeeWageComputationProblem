f942b7e0c71beb083307146582b009d5d3c4e3f5
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 20:24:04 +0530

    [vidyadhar hudge]Add:To check employee is prsent or absent

--- EmployeeWageComputationProblems/Program.cs
+++ EmployeeWageComputationProblems/Program.cs
@@ -7,6 +7,19 @@ namespace EmployeeWageComputationProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation program");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is prsent");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
+ 
