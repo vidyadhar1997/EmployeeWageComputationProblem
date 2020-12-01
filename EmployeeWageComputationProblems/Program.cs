65a65b4bc8f2000d0e5c9d3ce7d3c49abd9618ea
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 01:06:58 +0530

    [vidyadhar hudge]Add:Multiple company using interface approach

--- EmployeeWageComputationProblems/Program.cs
+++ EmployeeWageComputationProblems/Program.cs
@@ -6,12 +6,10 @@ namespace EmployeeWageComputationProblems
     {
         static void Main(string[] args)
         {
-            EmpWageBuilderObject dMart=new EmpWageBuilderObject("DMart",20,2,10);
-            EmpWageBuilderObject reliance=new EmpWageBuilderObject("Reliance",10,4,20);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.toString());
-            reliance.computeEmpWage();
-            Console.WriteLine(reliance.toString());
+            EmpWageBuilderArray empWageBuilder=new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart",20,2,10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
