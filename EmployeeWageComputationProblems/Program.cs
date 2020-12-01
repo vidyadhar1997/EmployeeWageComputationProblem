54066affc5496b168f3977b08fda06f11af89a06
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 21:15:08 +0530

    [vidyadhar hudge]Add:Calculated part time emp wage

--- EmployeeWageComputationProblems/Program.cs
+++ EmployeeWageComputationProblems/Program.cs
@@ -7,17 +7,20 @@ namespace EmployeeWageComputationProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation program");
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
-          
-
             }
             else
             {
