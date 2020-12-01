55665beda0eaf11eec9d03f19a0b37e152f3d605
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 22:40:07 +0530

    [vidyadhar hudge]Add:Calculated wages till condtion of total working hour & day

--- EmployeeWageComputationProblems/Program.cs
+++ EmployeeWageComputationProblems/Program.cs
@@ -7,14 +7,16 @@ namespace EmployeeWageComputationProblems
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HOURS_IN_MONTH = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation program");
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWages = 0;
-            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
+            int totalEmpWages;
+            int totalWorkingDays = 0;
+            int totalEmployeeHour = 0;
+            while(empHrs<=MAX_HOURS_IN_MONTH && totalWorkingDays<= NUM_OF_WORKING_DAYS)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
@@ -30,10 +32,12 @@ namespace EmployeeWageComputationProblems
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWages = totalEmpWages + empWage;
-                Console.WriteLine("Employee wages is=" + empWage);
+
+                totalEmployeeHour+=empHrs;
+                Console.WriteLine("Day="+ totalWorkingDays + " Emplyee hours="+ empHrs);
+                totalWorkingDays++;
             }
+            totalEmpWages = totalEmployeeHour * EMP_RATE_PER_HOUR;
             Console.WriteLine("total employee wage is=" +totalEmpWages);
         }
     }
