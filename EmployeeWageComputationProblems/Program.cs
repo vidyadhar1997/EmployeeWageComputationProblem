dc54fde5aab8345f7f466a5c918985292a31ba04
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 23:11:28 +0530

    [vidyadhar hudge]Refactor:using class methode

--- EmployeeWageComputationProblems/Program.cs
+++ EmployeeWageComputationProblems/Program.cs
@@ -8,16 +8,15 @@ namespace EmployeeWageComputationProblems
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HOURS_IN_MONTH = 20;
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to employee wage computation program");
+        public const int MAX_HOURS_IN_MONTH = 10;
+        public static int computeEmpWage()
+        { 
             int empHrs = 0;
-            int totalEmpWages;
             int totalWorkingDays = 0;
             int totalEmployeeHour = 0;
             while(empHrs<=MAX_HOURS_IN_MONTH && totalWorkingDays<= NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -32,13 +31,17 @@ namespace EmployeeWageComputationProblems
                         empHrs = 0;
                         break;
                 }
-
                 totalEmployeeHour+=empHrs;
                 Console.WriteLine("Day="+ totalWorkingDays + " Emplyee hours="+ empHrs);
-                totalWorkingDays++;
             }
-            totalEmpWages = totalEmployeeHour * EMP_RATE_PER_HOUR;
+            int totalEmpWages = totalEmployeeHour * EMP_RATE_PER_HOUR;
             Console.WriteLine("total employee wage is=" +totalEmpWages);
+            return totalEmpWages;    
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+            computeEmpWage();
         }
     }
 }
