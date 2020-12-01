292930d799a35a7c83bd33c42af0838d685089b4
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 21:43:35 +0530

    [vidyadhar hudge]Add:Calculated monthely and total wages

--- EmployeeWageComputationProblems/Program.cs
+++ EmployeeWageComputationProblems/Program.cs
@@ -7,28 +7,34 @@ namespace EmployeeWageComputationProblems
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation program");
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpWages = 0;
+            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWages = totalEmpWages + empWage;
+                Console.WriteLine("Employee wages is=" + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wages is=" + empWage);
+            Console.WriteLine("total employee wage is=" +totalEmpWages);
         }
     }
 }
- 
