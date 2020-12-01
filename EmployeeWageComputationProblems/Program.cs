802a2377ab0d41a12fc7f3f2373b4935cc2309fe
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 20:19:55 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ EmployeeWageComputationProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+        }
+    }
+}
