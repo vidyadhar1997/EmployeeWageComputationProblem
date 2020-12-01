146e9f732fc2ba0203d3566b10b57199b2099ab1
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-01 20:39:21 +0530

    [vidyadhar hudge]Add:Calculated Daily employee wage

--- EmployeeWageComputationProblems/Program.cs
+++ EmployeeWageComputationProblems/Program.cs
@@ -8,17 +8,23 @@ namespace EmployeeWageComputationProblems
         {
             Console.WriteLine("Welcome to employee wage computation program");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is prsent");
+                empHrs = 8;
+          
 
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wages is=" + empWage);
         }
     }
 }
