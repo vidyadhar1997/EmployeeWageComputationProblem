d37ed9267f972bf989ee44d42880f83aa90a64c8
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 16:43:39 +0530

    [vidyadhar hudge]Refactor:Store wages along with daily wages

--- EmployeeWageComputationProblems/EmpWageBuilderArray.cs
+++ EmployeeWageComputationProblems/EmpWageBuilderArray.cs
@@ -8,18 +8,21 @@ namespace EmployeeWageComputationProblems
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        private int numOfCompany = 0;
+        
         LinkedList<CompanyEmpWage> companyEmpWageList;
+        Dictionary<string, CompanyEmpWage> companyToEmpWageMap;
 
         public EmpWageBuilderArray()
         {
             this.companyEmpWageList = new LinkedList<CompanyEmpWage>();
+            this.companyToEmpWageMap = new Dictionary<string, CompanyEmpWage>();
         }
 
         public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
             CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
             this.companyEmpWageList.AddLast(companyEmpWage);
+            this.companyToEmpWageMap.Add(company,companyEmpWage);
         }
 
         public void computeEmpWage()
