f8cdc6e7b70ac7b79e610b9d8ce7891f8613a026
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 15:41:42 +0530

    [vidyadhar hudge]Refactor:Use of linked list instead of array

--- EmployeeWageComputationProblems/EmpWageBuilderArray.cs
+++ EmployeeWageComputationProblems/EmpWageBuilderArray.cs
@@ -9,25 +9,25 @@ namespace EmployeeWageComputationProblems
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         private int numOfCompany = 0;
-        private CompanyEmpWage[] companyEmpWageArray;
+        LinkedList<CompanyEmpWage> companyEmpWageList;
 
         public EmpWageBuilderArray()
         {
-            this.companyEmpWageArray = new CompanyEmpWage[5];
+            this.companyEmpWageList = new LinkedList<CompanyEmpWage>();
         }
 
         public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
-            numOfCompany++;
+            CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            this.companyEmpWageList.AddLast(companyEmpWage);
         }
 
         public void computeEmpWage()
         {
-            for (int i = 0; i < numOfCompany; i++)
+            foreach (CompanyEmpWage companyEmpWage in this.companyEmpWageList)
             {
-                companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(this.companyEmpWageArray[i]));
-                Console.WriteLine(this.companyEmpWageArray[i].toString());
+                companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+                Console.WriteLine(companyEmpWage.toString());
             }
         }
 
